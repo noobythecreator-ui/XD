0a2bd6c5b993e2f2ca8537660dce1a99f8e92cff
noobythecreator-ui <atuspariadarek@gmail.com>
2026-06-28 15:33:20 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,22 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/MenuLayer.hpp>
-#include <geode/ui/GeodeUI.hpp>
 
 using namespace geode::prelude;
 
-class $modify(MenuLayerHook, MenuLayer) {
+// Nos acoplamos a la pantalla principal del juego (MenuLayer)
+class $modify(MenuLayer) {
     bool init() {
-        if (!MenuLayer::init()) return false;
-
-        auto menuPrincipal = this->getChildByID("bottom-menu"); 
-        if (!menuPrincipal) menuPrincipal = this->getChildByID("main-menu");
-
-        if (menuPrincipal) {
-            auto miBotonMenu = CCMenuItemSpriteExtra::create(
-                CircleButtonSprite::createWithSpriteFrameName("GJ_plusBtn_001.png", 1.0f, CircleBaseColor::Green),
-                this,
-                menu_selector(MenuLayerHook::onAbrirMiMenu)
-            );
-
-            menuPrincipal->addChild(miBotonMenu);
-            menuPrincipal->updateLayout();
+        if (!MenuLayer::init()) {
+            return false;
         }
 
-        return true;
-    }
+        // Esto mandará un mensaje de saludo flotante al abrir Geometry Dash
+        FLAlertLayer::create(
+            "¡Hola desde Geode!",
+            "Tu mod menu <color_l>xdmod</color_l> se cargo con exito.",
+            "Aceptar"
+        )->show();
 
-    void onAbrirMiMenu(CCObject* sender) {
-        geode::openSettingsPopup(Mod::get());
+        return true;
     }
 };
