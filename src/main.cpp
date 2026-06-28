6f1f3e05f35e54bc74f51a9979a0424e78be73c4
noobythecreator-ui <atuspariadarek@gmail.com>
2026-06-28 15:34:49 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,16 @@
 
 using namespace geode::prelude;
 
-// Nos acoplamos a la pantalla principal del juego (MenuLayer)
 class $modify(MenuLayer) {
     bool init() {
         if (!MenuLayer::init()) {
             return false;
         }
 
-        // Esto mandará un mensaje de saludo flotante al abrir Geometry Dash
         FLAlertLayer::create(
-            "¡Hola desde Geode!",
-            "Tu mod menu <color_l>xdmod</color_l> se cargo con exito.",
-            "Aceptar"
+            "Geode",
+            "Mod loaded successfully",
+            "OK"
         )->show();
 
         return true;
